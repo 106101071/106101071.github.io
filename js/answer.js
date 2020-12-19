bfea0f6491cada47d3bc4112d75e75264ac105f6
106101071 <74147031+106101071@users.noreply.github.com>
2020-12-19 18:53:09 +0800

    Add files via upload

--- js/answer.js
+++ js/answer.js
@@ -20,30 +20,6 @@ window.onload = () =>{
         else
         {
             window.location.replace('/face.html');
-            // leftEar = JSON.parse(localStorage.getItem('leftEar'));
-            // switch (leftEar.count) {
-            //     case '0':
-            //         console.log(0);
-            //         break;
-            //     case '1':
-            //         console.log(1);
-            //         break;
-            //     case '2':
-            //         console.log(2);
-            //         break;
-            //     case '3':
-            //         console.log(3);
-            //         break;
-            //     case '4':
-            //         console.log(4);
-            //         break;
-            //     case '5':
-            //         console.log(5);
-            //         break;
-            //     default:
-            //         console.log('error');
-            //         break;
-            // }
         }
     }
 }
